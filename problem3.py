3e10bba77cf0349b5156500febba878293b6a0b7
ciphernix <ciphernix@gmail.com>
2014-05-25 19:53:46 -0400

    Problem3

--- problem3.py
+++ problem3.py
@@ -1,34 +1,20 @@
 #!/usr/bin/python
-''' Find's the highest prime factor using Fermat's Factorization'''
-
-import math
 import sys
 
-def is_square(n):
-    '''Returns true if n is a perfect squre.'''
-
-    sqrt_n = math.floor(math.sqrt(n))
-    return n == sqrt_n * sqrt_n
-
-def find_prime_factor(n):
-    '''Find largest prime factor.'''
-
-    if n % 2 == 0:
-        n = n +1
-        sys.exit(-1)
-
-    sqrt_n = math.sqrt(n)
-    a = math.ceil(sqrt_n)
-    b2 = a*a - n
-
-    while not is_square(b2):
-        #a += 1
-        #b2 = a*a - n
-        b2 = b2 + 2*a + 1
-        a = a + 1
-
-    return a + math.sqrt(b2)
+def find_prime(n):
+    i = 2
+    while i * i < n:
+        while n % i == 0:
+            n = n / i
+        i = i + 1
+    return n
 
 if __name__ == "__main__":
-    number = int(raw_input("Number >> "))
-    print find_prime_factor(number)
+    number = raw_input("Number>>")
+    try:
+        number = int(number)
+    except ValueError,TypeError:
+        print "Not a valid number{0}".format(number)
+        sys.exit(-1)
+
+    print find_prime(number)
